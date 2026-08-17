d571ff7e325be3e5d83f49a332596033bceef25e
Mtank2006 <the.mtank2006@gmail.com>
2026-08-17 20:55:00 +0530

    LC 0122 — Greedy accumulation of positive price differences

---
+++ LeetCode-Main/0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int size = prices.size();
+        int buy = prices[0];
+        int profit = 0;
+        for (size_t i = 1; i < size ; ++i) {
+            if (prices[i] < buy) {
+                buy = prices[i];
+            }
+            else {
+                profit += prices[i] - buy;
+                buy = prices[i];
+            }
+        }
+        return profit;
+    }
+};
